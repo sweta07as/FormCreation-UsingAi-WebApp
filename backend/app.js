ee175ded04ec3e1feb4a15cd59dcd1b7f96aae10
sweta07as <swetaagrawal71099@gmail.com>
2024-05-22 16:04:22 +0530

    changed server.js

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,9 @@ const cors = require("cors");
 const app = express();
 
 dotenv.config({ path: "config/config.env" });
-
 app.use(express.json());
+
+ 
 app.use(express.urlencoded({ extended: true })); //added later
 app.use(cors());
 
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
 const app = require("./app");
-
+const express = require("express");
 
 const dotenv = require("dotenv");
 const connectDatabase = require("./database");
 
 //config
 dotenv.config({ path: "config/config.env" });
+
+
+//deployment
+const path = require("path");
+const _dirname = path.dirname("");
+const buildpath = path.join(_dirname, "../frontend/build");
+app.use(express.static(buildpath));
+//
 
 //Connecting to database
 connectDatabase();
